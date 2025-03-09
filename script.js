6b8e0cf332ef41e0ae8c5db5ad3ff161c4369aa5
raja16778 <sanjayking215488@gmail.com>
2025-03-09 21:07:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+// First Page Button
 function goToHoliPage() {
-    window.location.href = "holi-page.html";
+    window.location.href = "holi.html";
 }
 
+// Play Fireworks Sound
 function showFireworks() {
     alert("🎆🎇 पटाखे फूट रहे हैं! 🎇🎆");
     let audio = new Audio("fireworks.mp3");
     audio.play();
 }
 
+// Auto Play Holi Song
 function playHoliSong() {
     document.getElementById("holiSong").play();
 }
 
+// Open WhatsApp
 function openWhatsApp() {
     window.location.href = "https://wa.me/9569739986";
 }
